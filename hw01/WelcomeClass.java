b35b2e87f8fb27c52dcd4790e417722269c7699c
delrinj <delrinj@yahoo.com>
2015-08-30 21:26:01 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+///////////////////////////////////
+//Eric DelGizzo
+//Demo 0
+//Welcome Class Java Program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+// define a class
+public class WelcomeClass{
+    
+// add a main method
+    public static void main(String[]agrs){
+        
+// pring statement with the box around it
+        System.out.println("   -----------");
+        System.out.println("   | Welcome |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-E--S--D--2--1--19->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("Over the summer I hiked at Arcadia National Park with my cousins.");
+    }
+    
+    // test/testtest
+}
